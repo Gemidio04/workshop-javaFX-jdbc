98197f70c700ba03617aed568bce4ed991f63d4f
Gustavo Emídio <gemidio2004@gmail.com>
2024-07-02 16:17:53 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -19,78 +22,92 @@ import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 
-	private Department entity; 
-	
-	private DepartmentService depService; 
+	private Department entity;
+
+	private DepartmentService depService;
+
+	private List<DataChangeListener> dataChangeListeners = new ArrayList(); 
 	
 	@FXML
-	private TextField txtId; 
-	
+	private TextField txtId;
+
 	@FXML
 	private TextField txtName;
-	
+
 	@FXML
 	private Label labelErrorName;
-	
+
 	@FXML
 	private Button btSave;
-	
+
 	@FXML
 	private Button btCancel;
-	
+
 	public void setDepartment(Department entity) {
-		this.entity=entity; 	
+		this.entity = entity;
 	}
-	
+
 	public void setDepartmentService(DepartmentService depService) {
 		this.depService = depService;
-	}	
+	}
 	
+	public void subcribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener); 
+	}
+
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
-		if(entity==null) {
-			throw new IllegalStateException("Entity was null"); 
-		}if(depService==null) {
-			throw new IllegalStateException("Service was null"); 
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (depService == null) {
+			throw new IllegalStateException("Service was null");
 		}
 		try {
 			entity = getFormData();
 			depService.saveOrUpdate(entity);
+			notifyDataChangeListeners(); 
 			Utils.currentStage(event).close();
-			
-		}catch(DbException ex) {
+
+		} catch (DbException ex) {
 			Alerts.showAlert("Error saving object", null, ex.getMessage(), AlertType.ERROR);
-			}
 		}
-	
+	}
+
+	private void notifyDataChangeListeners() {
+		for(DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+	}
+
 	private Department getFormData() {
-		Department dep = new Department(); 
+		Department dep = new Department();
 		dep.setId(Utils.tryParseToInt(txtId.getText()));
 		dep.setName(txtName.getText());
-		return dep; 
+		return dep;
 	}
 
 	@FXML
 	public void onBtCancelAction(ActionEvent event) {
 		Utils.currentStage(event).close();
 	}
-	
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		initializeNodez();
 	}
-	
+
 	private void initializeNodez() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
 	}
-	
+
 	public void updateFormData() {
-		if(entity == null) {
-			throw new IllegalStateException("Entity was null"); 
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
 	}
-	
+
 }
